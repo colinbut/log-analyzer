9a1ff011844b139b7ff5c18a7f57d6b899531c1a
Colin But <colin.but@outlook.com>
2021-09-19 14:30:54 +0100

    Initial feature commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/colinbut/log-analyzer
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	INFO             = "INFO"
+	DEBUG            = "DEBUG"
+	WARNING          = "WARNING"
+	ERROR            = "ERROR"
+	TRACE            = "TRACE"
+	DEFAULT_LOG_NAME = "example.log"
+)
+
+func initLogLevelCountMap(logLevelCount map[string]int) {
+	logLevelCount[INFO] = 0
+	logLevelCount[DEBUG] = 0
+	logLevelCount[WARNING] = 0
+	logLevelCount[ERROR] = 0
+	logLevelCount[TRACE] = 0
+}
+
+func updateLogLevelCount(logLevelCount map[string]int, line string) {
+	for key := range logLevelCount {
+		if strings.Contains(line, key) {
+			count := logLevelCount[key]
+			count++
+			logLevelCount[key] = count
+			break
+		}
+	}
+}
+
+func main() {
+	logLevelCount := make(map[string]int)
+	initLogLevelCountMap(logLevelCount)
+
+	logFile := flag.String("logFile", DEFAULT_LOG_NAME, "The log file path to analyze")
+
+	flag.Parse()
+
+	file, err := os.Open(*logFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// fmt.Println(line)
+		updateLogLevelCount(logLevelCount, line)
+	}
+
+	// log output to console
+	for key, value := range logLevelCount {
+		fmt.Println(key, value)
+	}
+
+}
